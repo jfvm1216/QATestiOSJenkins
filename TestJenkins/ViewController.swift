d4f7aede214e5ba0e375e3cc57bf67670f19b8c8
Adrián de Jesús  Piedra Goicochea <apiedra@ia.com.mx>
2016-12-14 18:17:11 -0600

    Primer commit, pruebas Jenkins

---
+++ TestJenkins/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Adrián de Jesús  Piedra Goicochea on 14/12/16.
+//  Copyright © 2016 IA Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
